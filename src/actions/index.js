6d78d6de252e5d51fe9d180fb00390be014a8100
Jennifer Kramer <jenkrame@gmail.com>
2021-01-22 15:26:43 -0500

    Working on handleSubmit for adding new smurf.

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,22 +12,34 @@ export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE";
 export const SET_NEW_SMURF = "SET_NEW_SMURF";
 export const SET_ERROR = "SET_ERROR";
 
+//Task List:
+//1. Add fetch smurfs action: 
+//              - fetch and return initial list of smurfs
+//              - dispatch actions that indicate if we are waiting for a server response
+//              - dispatch an error text action if an error is returned from the server
+
 export const getSmurfs = () => (dispatch) => {
     dispatch({ type: START_CALL });
 
     axios
         .get('http://localhost:3333/smurfs')
         .then((res) => {
-            console.log("Get Smurf Success Action: ", res.data);
+            // console.log("Get Smurf Success Action: ", res.data);
             dispatch({ type: CALL_SUCCESS, payload: res.data });
         })
         .catch((err) => {
-            console.log("Get Smurf Failure Action: ", err);
+            // console.log("Get Smurf Failure Action: ", err);
             dispatch({ type: CALL_FAILURE, payload: err });
         });
 };
 
-export const postSmurfs = (newSmurf) => (dispatch) => {
+//2. Add add smurf action:
+//              - dispatch an error text action if smurf data does not includes a name, nickname and position field
+//              - send a post request with the smurf as body to see if there is an error
+//              - dispatch add smurf action if request is successful
+//              - dispatch an error text action if an request returns an error
+
+export const addSmurf = (newSmurf) => (dispatch) => {
     dispatch({ type: START_ADD_SMURF });
 
     axios
@@ -40,16 +52,11 @@ export const postSmurfs = (newSmurf) => (dispatch) => {
         });
 };
 
-//Task List:
-//1. Add fetch smurfs action: 
-//              - fetch and return initial list of smurfs
-//              - dispatch actions that indicate if we are waiting for a server response
-//              - dispatch an error text action if an error is returned from the server
-//2. Add add smurf action:
-//              - dispatch an error text action if smurf data does not includes a name, nickname and position field
-//              - send a post request with the smurf as body to see if there is an error
-//              - dispatch add smurf action if request is successful
-//              - dispatch an error text action if an request returns an error
+export const errorMessage = (error) => (dispatch) => {
+    dispatch({ type: SET_ERROR, payload: error });
+};
+
+
 //3. Add set error text action:
 //              - return action object setting error text
 //4. Any other actions you deem nessiary to complete application.
--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,50 +1,76 @@
+//Task List:
+//1. Add in all necessary import components and library methods.
 import React from 'react';
+import { connect } from 'react-redux';
+import { addSmurf } from '../actions';
 
 class AddForm extends React.Component {
 
     state = {
-        id: Date.now(),
-        name: '',
-        position: '',
-        nickname: '',
-        description: ''
+        newSmurf: {
+            id: Date.now(),
+            name: '',
+            position: '',
+            nickname: '',
+            description: ''
+        }
     };
 
     handleChange = (e) => {
-        console.log("Handle Change: ", e.target.name);
+        // console.log("Handle Change: ", e.target.name);
         this.setState({
-            ...this.state.form,
-            [e.target.name]: e.target.value
+            ...this.state,
+            newSmurf: {
+                ...this.state.newSmurf,
+                [e.target.name]: e.target.value
+            }
         });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addSmurf(this.state.newSmurf);
     };
 
     render() {
         return(<section>
             <h2>Add Smurf</h2>
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="name">Name:</label><br/>
                     <input onChange={this.handleChange} name="name" id="name" value={this.state.name} />
+
                     <label htmlFor="name">Position:</label><br/>
                     <input onChange={this.handleChange} name="position" id="position" value={this.state.position}/>
+
                     <label htmlFor="name">Nickname:</label><br/>
                     <input onChange={this.handleChange} name="nickname" id="nickname" value={this.state.nickname}/>
+                    
                     <label htmlFor="name">Description:</label><br/>
                     <input onChange={this.handleChange} name="description" id="description" value={this.state.description}/>
                 </div>
 
-                <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: </div>
+                {(this.props.error) &&
+                    <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {this.props.error}</div>
+                }
                 <button>Submit Smurf</button>
             </form>
         </section>);
     }
-}
+};
 
-export default AddForm;
+const mapStateToProps = state => {
+    return {
+        error: state.error
+    }
+};
 
-//Task List:
-//1. Add in all necessary import components and library methods.
+
 //2. Connect all needed redux state props and action functions to the component before exporting.
+export default connect(mapStateToProps, { addSmurf })(AddForm);
+
+
+
 //3. Add state holding name, position, nickname and description to component.
 //4. Build form DOM to include inputs for name, position and description of the component.
 //      - an array of smurfs
